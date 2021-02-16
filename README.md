a547c1cf87200a95a4c632073fb5ad07c5c91b01
Yue Hu <sammo3182@sina.com>
2021-02-16 17:10:09 +0800

    pkgdown site

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
+<!-- badges: start -->
+
 [![CRAN version](http://www.r-pkg.org/badges/version/drhutools)](https://cran.r-project.org/package=drhutools) ![](http://cranlogs.r-pkg.org/badges/grand-total/drhutools)
+
+[![R-CMD-check](https://github.com/sammo3182/drhutools/workflows/R-CMD-check/badge.svg)](https://github.com/sammo3182/drhutools/actions)
+<!-- badges: end -->
 
 ------------------------------------------------------------------------
 drhutools
--- dev/checkProcess.R
+++ dev/checkProcess.R
@@ -1,5 +1,6 @@
 # Setup
 
+
 library(devtools)
 library(roxygen2)
 
@@ -14,3 +15,14 @@ spell_check()
 rhub::validate_email(email = "yuehu@tsinghua.edu.cn", token = "345011f4ca60404abf76007e9ae89e3e")
 
 check_rhub(email = "yuehu@tsinghua.edu.cn")
+
+
+# Package website building
+
+usethis::git_vaccinate() #Adds .DS_Store, .Rproj.user, .Rdata, .Rhistory, and .httr-oauth to your global (a.k.a. user-level) .gitignore. This is good practice as it decreases the chance that you will accidentally leak credentials to GitHub.
+
+use_github_links()
+use_github_action_check_standard()
+
+library(pkgdown)
+build_site()
