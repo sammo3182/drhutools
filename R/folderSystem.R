e4231db61df405cb9ca9950469f5ef5af8265b9f
DengWen <dengwenjoy@outlook.com>
2024-08-08 10:18:33 +0800

    1. update the folderSystem funcion. 2. delete the template_cn.docx

--- R/folderSystem.R
+++ R/folderSystem.R
@@ -3,7 +3,7 @@
 #' 
 #' @details The system contains specific places to store codes, output, data, manuscript (referred images and submission versions), and miscellaneous documents. It helps researchers to organize materials and drafts during the whole process of academic writing from data collection to manuscript publication. This is the recommanded folder structure for cooperative projects with members of Dr.Hu's Amazing Team.
 #' 
-#' @importFrom purrr walk here fs
+#' @importFrom purrr walk fs
 #' 
 #' @return A set of folders under the working directory.
 #' 
@@ -31,28 +31,22 @@ folderSystem <- function() {
       "paper/submission/submitted files here; delete this file when locating the real files here.txt.md", 
       "document",
       "document/documents and materials; delete this file when locating the real files here.txt.md")
-  
-  template_path <- here("template_cn.docx")
-  
+
   walk(ls_dir, function(path) {
-    if (grepl("\\.\\w+$", path)) {
-      dir_name <- dirname(path)
-      if (!dir.exists(dir_name)) {
-        dir.create(dir_name, recursive = TRUE)
-      }
-      if (!file.exists(path)) {
-        if (basename(path) == "template_cn.docx") {
-          file_copy(template_path, path)
-        } else {
-          file_create(path)
-        }
-      }
-    } else {
-      if (!dir.exists(path)) {
-        dir.create(path, recursive = TRUE)
-      }
+  if (grepl("\\.\\w+$", path)) {
+    dir_name <- dirname(path)
+    if (!dir.exists(dir_name)) {
+      dir.create(dir_name, recursive = TRUE)
     }
-  })
+    if (!file.exists(path)) {
+      file.create(path)
+    }
+  } else {
+    if (!dir.exists(path)) {
+      dir.create(path, recursive = TRUE)
+    }
+  }
+})
 }
 
 folderSystem()
